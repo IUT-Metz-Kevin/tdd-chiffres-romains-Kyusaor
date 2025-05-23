0fdac1b5c261055b4505a810316c92657a4f6c90
Kyusaor <kyu.chrysaor@gmail.com>
2025-05-23 11:01:26 +0200

    Passage tests initiaux

--- src/modeles/RomanNumeral.class.ts
+++ src/modeles/RomanNumeral.class.ts
@@ -1,5 +1,15 @@
 export class RomanNumeral {
   of(value: string): number {
+    return RomanNumeral.Alphabet[value];
+  }
 
+  static Alphabet = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
   }
 }
--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -9,35 +9,35 @@ import { RomanNumeral } from './modeles/RomanNumeral.class.js';
 
 it('Should return 1', () => {
   const rom = new RomanNumeral();
-  expect(rom.of("I")).toBe(1);
+  expect(rom.of('I')).toBe(1);
 });
 
 it('Should return 5', () => {
   const rom = new RomanNumeral();
-  expect(rom.of("V")).toBe(5);
+  expect(rom.of('V')).toBe(5);
 });
 
 it('Should return 10', () => {
   const rom = new RomanNumeral();
-  expect(rom.of("X")).toBe(10);
+  expect(rom.of('X')).toBe(10);
 });
 
 it('Should return 50', () => {
   const rom = new RomanNumeral();
-  expect(rom.of("L")).toBe(50);
+  expect(rom.of('L')).toBe(50);
 });
 
 it('Should return 100', () => {
   const rom = new RomanNumeral();
-  expect(rom.of("C")).toBe(100);
+  expect(rom.of('C')).toBe(100);
 });
 
 it('Should return 500', () => {
   const rom = new RomanNumeral();
-  expect(rom.of("D")).toBe(500);
+  expect(rom.of('D')).toBe(500);
 });
 
 it('Should return 1000', () => {
   const rom = new RomanNumeral();
-  expect(rom.of("M")).toBe(1000);
+  expect(rom.of('M')).toBe(1000);
 });
