ae6610bd65efc60c026d4d455ee40cb8fc86d5c6
Kyusaor <kyu.chrysaor@gmail.com>
2025-05-23 11:53:57 +0200

    passage des tests pour traiter le découpage des nombres

--- src/modeles/RomanNumeral.class.ts
+++ src/modeles/RomanNumeral.class.ts
@@ -9,21 +9,39 @@ export class RomanNumeral {
     M: 1000,
   };
 
-  of(value: string): number {
-    const keys = Object.keys(RomanNumeral.Alphabet);
+  of(numberString: string): number {
+    const baseNumberKeys = Object.keys(RomanNumeral.Alphabet);
+    let individualNumbers: string[] = [];
 
-    if (keys.indexOf(value[0]) < keys.indexOf(value[1]))
-      return this.ofChar(value[1]) - this.ofChar(value[0]);
-    else {
-      const initialization = 0;
-      return Array.from(value).reduce(
-        (accu: any, currentValue: string) => accu + this.ofChar(currentValue),
-        initialization,
-      );
+    while (numberString.length > 0) {
+      const substractionCheck =
+        baseNumberKeys.indexOf(numberString[0]) <
+        baseNumberKeys.indexOf(numberString[1]);
+
+      if (substractionCheck) {
+        individualNumbers.push([numberString[0], numberString[1]].join(''));
+        numberString = numberString.slice(2);
+      } else {
+        individualNumbers.push(numberString[0]);
+        numberString = numberString.slice(1);
+      }
     }
+
+    const initialization = 0;
+    return individualNumbers.reduce(
+      (accu: number, currentValue: string) => accu + this.ofChar(currentValue),
+      initialization,
+    );
   }
 
-  private ofChar(value: string) {
-    return RomanNumeral.Alphabet[value];
+  private ofChar(numberString: string) {
+    const baseNumberKeys = Object.keys(RomanNumeral.Alphabet);
+
+    if (
+      baseNumberKeys.indexOf(numberString[0]) <
+      baseNumberKeys.indexOf(numberString[1])
+    )
+      return this.ofChar(numberString[1]) - this.ofChar(numberString[0]);
+    else return RomanNumeral.Alphabet[numberString];
   }
 }
