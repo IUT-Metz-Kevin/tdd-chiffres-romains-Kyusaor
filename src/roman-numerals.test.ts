bec72507dace6c88ef14e022e7191c8b3da2b8a0
Kyusaor <kyu.chrysaor@gmail.com>
2025-05-23 11:08:09 +0200

    reecriture des tests en test each

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -7,57 +7,20 @@
  */
 import { RomanNumeral } from './modeles/RomanNumeral.class.js';
 
-it('Should return 1', () => {
+test.each([
+  ['I', 1],
+  ['V', 5],
+  ['X', 10],
+  ['L', 50],
+  ['C', 100],
+  ['D', 500],
+  ['M', 1000],
+  ['IV', 4],
+  ['IX', 9],
+  ['XL', 40],
+  ['XC', 90],
+])('%s should give %s', (input: string, expected: number) => {
+  console.log(input, expected);
   const rom = new RomanNumeral();
-  expect(rom.of('I')).toBe(1);
-});
-
-it('Should return 5', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('V')).toBe(5);
-});
-
-it('Should return 10', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('X')).toBe(10);
-});
-
-it('Should return 50', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('L')).toBe(50);
-});
-
-it('Should return 100', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('C')).toBe(100);
-});
-
-it('Should return 500', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('D')).toBe(500);
-});
-
-it('Should return 1000', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('M')).toBe(1000);
-});
-
-it('Should return 4', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('IV')).toBe(4);
-});
-
-it('Should return 9', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('IV')).toBe(4);
-});
-
-it('Should return 40', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('XL')).toBe(40);
-});
-
-it('Should return 90', () => {
-  const rom = new RomanNumeral();
-  expect(rom.of('XC')).toBe(90);
+  expect(rom.of(input)).toBe(expected);
 });
